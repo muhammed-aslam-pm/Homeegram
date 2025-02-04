3251a45da9a3f8d2c076f97b54e5fe6b9a502b88
muhammed-aslam-pm <muhammedaslampm707@gmail.com>
2025-02-04 22:14:30 +0530

    logger updated

--- lib/core/network/dio_interceptors.dart
+++ lib/core/network/dio_interceptors.dart
@@ -10,25 +10,40 @@ class LoggerInterceptor extends Interceptor {
   void onError(DioException err, ErrorInterceptorHandler handler) {
     final options = err.requestOptions;
     final requestPath = '${options.baseUrl}${options.path}';
-    logger.e('${options.method} request ==> $requestPath'); //Error log
-    logger.d('Error type: ${err.error} \n '
-        'Error message: ${err.message}'); //Debug log
-    handler.next(err); //Continue with the Error
+
+    logger.e(
+        '⛔ ERROR on ${options.method} request ==> $requestPath'); // Error log
+    logger.d('📤 REQUEST BODY: ${options.data}'); // Log request body
+
+    if (err.response != null) {
+      logger.e('📥 RESPONSE STATUS CODE: ${err.response?.statusCode}');
+      logger.e('📝 RESPONSE MESSAGE: ${err.response?.statusMessage}');
+      logger.e('📊 RESPONSE DATA: ${err.response?.data}');
+    } else {
+      logger.e('❌ No response received. Possible network issue.');
+    }
+
+    handler.next(err); // Continue with the error
   }
 
   @override
   void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
     final requestPath = '${options.baseUrl}${options.path}';
-    logger.i('${options.method} request ==> $requestPath'); //Info log
-    handler.next(options); // continue with the Request
+    logger.i('💡 ${options.method} request ==> $requestPath'); // Info log
+    logger.d(
+        '📨 Headers: ${options.headers} \n 🔍 Data: ${options.data}'); // Debug log
+    handler.next(options); // Continue with the request
   }
 
   @override
   void onResponse(Response response, ResponseInterceptorHandler handler) {
-    logger.d('STATUSCODE: ${response.statusCode} \n '
-        'STATUSMESSAGE: ${response.statusMessage} \n'
-        'HEADERS: ${response.headers} \n'
-        'Data: ${response.data}'); // Debug log
-    handler.next(response); // continue with the Response
+    final requestPath =
+        '${response.requestOptions.baseUrl}${response.requestOptions.path}';
+    logger.i('✅ Response from ==> $requestPath'); // Info log
+    logger.d('📜 STATUS CODE: ${response.statusCode} \n '
+        '📝 STATUS MESSAGE: ${response.statusMessage} \n'
+        '📩 HEADERS: ${response.headers} \n'
+        '📊 DATA: ${response.data}'); // Debug log
+    handler.next(response); // Continue with the response
   }
 }
